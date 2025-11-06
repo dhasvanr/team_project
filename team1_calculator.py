098970cfef0a2b687f01d31065ecfd60f1fdd3f7
dhasvan <dhasvanr583224104022@nprcolleges.org>
2025-11-06 15:02:13 +0530

    dhasvan

--- team1_calculator.py
+++ team1_calculator.py
@@ -1,4 +1,5 @@
-def add(a, b): return a + b
-def subtract(a, b): return a - b
-def multiply(a, b): return a * b
-def divide(a, b): return a / b if b != 0 else "Error: divide by zero"
+
+def multiply(a, b):
+    a= float(input("Enter the first number: "))
+    b= float(input("Enter the first number: "))
+    return a * b
